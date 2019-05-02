616c3adebc7d9b82bcc4a61aee2855d255eb42e0
Damien Beaufils <damien.beaufils@gmail.com>
2019-05-02 09:30:41 -0400

    moved loggedInUser check with exception at the beginning

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -4,8 +4,9 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Collections.emptyList;
 
 public class TripService {
 
@@ -16,16 +17,15 @@ public class TripService {
 	 * @throws UserNotLoggedInException
 	 */
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> foundTrips = new ArrayList<Trip>();
 		User loggedInUser = getLoggedInUser();
-		if (loggedInUser != null) {
-			if (user.isFriendWith(loggedInUser)) {
-				foundTrips = tripsBy(user);
-			}
-			return foundTrips;
-		} else {
+		if (loggedInUser == null) {
 			throw new UserNotLoggedInException();
 		}
+
+		if (user.isFriendWith(loggedInUser)) {
+			return tripsBy(user);
+		}
+		return emptyList();
 	}
 
 	List<Trip> tripsBy(User u) {
